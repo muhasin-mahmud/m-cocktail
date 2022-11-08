b5c223babd10cc38736435831e63737bb46ad441
Muhasin Mahmud <muhasinuddinmahmud@gmail.com>
2022-11-08 15:45:02 +0100

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore minima
+        exercitationem mollitia, totam enim magni neque fuga sint quos in
+        doloribus placeat aliquid temporibus laudantium nisi a ipsam magnam
+        adipisci. Consectetur doloribus obcaecati reiciendis.
+      </p>
+    </section>
+  );
+};
+
+export default About;
